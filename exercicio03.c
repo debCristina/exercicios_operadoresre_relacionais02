561ad397dc81c748b043ea53959171f8de24b0db
Debora_Cristina <133518001+Deboraxp@users.noreply.github.com>
2023-09-01 10:35:01 -0300

    Update exercicio03.c

--- exercicio03.c
+++ exercicio03.c
@@ -6,22 +6,22 @@ main(){
     int A= 7, B= 4, C=10;
 
     if((A < B) && (B < C)){
-        printf("Os valores em sua foma crescente e: %d, %d, %d", A, B, C);
+        printf("Os valores em sua foma ascendente e: %d, %d, %d", A, B, C);
 
     }else if((A < C) && (C< B)){
-        printf("Os valores em sua forma crescente e: %d, %d, %d", A, C, B);
+        printf("Os valores em sua forma ascendente e: %d, %d, %d", A, C, B);
 
     }else if((B < A) && (A < C)){
-        printf("Os valores em ordem crescente e: %d, %d, %d", B, A, C);
+        printf("Os valores em ordem ascendente e: %d, %d, %d", B, A, C);
 
     }else if((B < C) &&( C < A)){
-        printf("Os valores em ordem crescente e: %d, %d, %d", B, C, A);
+        printf("Os valores em ordem ascendente e: %d, %d, %d", B, C, A);
 
     }else if((C < A) && (A < B)){
-        printf("Os valores em ordem crescente e: %d, %d, %d", C, A, B);
+        printf("Os valores em ordem ascendente e: %d, %d, %d", C, A, B);
 
     }else{
-        printf("Os valores em ordem crescente e: %d, %d, %d", C, B, A);
+        printf("Os valores em ordem ascendente e: %d, %d, %d", C, B, A);
 
     }
 
