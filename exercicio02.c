663550329762f4ba02076df588c05cc2b06c0313
Debora_Cristina <133518001+Deboraxp@users.noreply.github.com>
2023-09-01 10:00:01 -0300

    Update exercicio02.c

--- exercicio02.c
+++ exercicio02.c
@@ -4,19 +4,37 @@ main(){
 
     //Construa um algoritmo que, dado quatro valores, A, B, C e D, o algoritmo imprima o maior e o menor valor. 
 
-    int A= 1, B= 2, C= 3, D= 4;
+    int A= 5, B= 6, C= 0, D= 18, maior, menor; 
 
-    if((A > B) && (A > C) && (A > D)){
-        printf("O maior numero e %d", A);
+    //verificar o maior de todos
+    if(A >B && A > C && A > D){
+        maior = A;
 
-    }else if((B > A) && (B > C) && (B > D)){
-        printf("O maior numero e o numero %d", B);
+    }else if(B > A && B > C && B > D){
+        maior = B;
 
-    }else if((C > A) && (C > B) && (C > D)){
-        printf("O maior numero e o numero %d", C);
+    }else if(C > A && C > B && C > D){
+        maior = C;
 
     }else{
-        printf("O maior numero e o numero %d", D);
+        maior = D;
+
     }
-    
+    //veficando o menor
+    if(A < B && A < C && A < D){
+        menor = A;
+
+    }else if(B < A && B < C && B < D){
+        menor = B;
+    }else if(C < A && C < B && C < D){
+        menor = C;
+    }else{
+        menor = D;
+    }
+    printf("Maior: %d,. Menor: %d", maior, menor);
+
 }
+    
+    
+    
+    
